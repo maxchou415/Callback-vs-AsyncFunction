c442797c411c1d9fe0fb8bdc83dd102d492784ca
Max Yi-Hsun Chou <hjes5209@icloud.com>
2017-02-27 00:29:06 +0800

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,12 +1,14 @@
-A.find({}, (err, aData) => {
+function exampleFunction () {
+  A.find({}, (err, aData) => {
   if(err) console.log(err)
-  B.find({}, (err, bData) => {
-    if(err) console.log(err)
-    C.find({}, (err, cData) {
+    B.find({}, (err, bData) => {
       if(err) console.log(err)
-      console.log('A', aData)
-      console.log('B', bData)
-      console.log('C', cData)
+      C.find({}, (err, cData) {
+        if(err) console.log(err)
+        console.log('A', aData)
+        console.log('B', bData)
+        console.log('C', cData)
+      })
     })
   })
-})
+}
